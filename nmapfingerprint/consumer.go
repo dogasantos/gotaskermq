27f314720f0b4a727a8c56a3f015081ae2a1fa16
Douglas <douglas.secco@gmail.com>
2021-07-21 09:41:04 -0300

    cpe

--- nmapfingerprint/consumer.go
+++ nmapfingerprint/consumer.go
@@ -58,8 +58,11 @@ func runNmapFingerprint(target string, ports string) string {
 
         for _, port := range host.Ports {
             //fmt.Printf("\tPort %d/%s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
-			fmt.Printf("\t%s:%d (name:%s Tunnel:%s Product:%s Version:%s \n\t\tcpe:%s\n",host.Addresses[0], port.ID, port.Service.Name, port.Service.Tunnel, port.Service.Product, port.Service.Version, port.Service.CPEs )
-
+			if port.Service.Tunnel == "ssl" {
+				fmt.Printf("\t%s:%d (name:%s Tunnel:%s Product:%s Version:%s \n\t\tcpe:%s\n",host.Addresses[0], port.ID, port.Service.Name, port.Service.Tunnel, port.Service.Product, port.Service.Version, port.Service.CPEs.cpe )
+			} else {
+				fmt.Printf("\t%s:%d (name:%s Product:%s Version:%s \n\t\tcpe:%s\n",host.Addresses[0], port.ID, port.Service.Name, port.Service.Product, port.Service.Version, port.Service.CPEs )
+			}
         }
     }
 
